cb70a3e410b07d5566b8f413dd8a732a2758c98a
hendesprit <hend.zormati@esprit.tn>
2025-01-28 12:24:14 +0100

    first commit

---
+++ src/main/java/tn/esprit/tpfoyer/entity/Reservation.java
@@ -0,0 +1,22 @@
+package tn.esprit.tpfoyer.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Reservation {
+    @Id
+    private String idReservation;
+    private Date anneeUniversitaire;
+    private Boolean estValide;
+}
---
+++ src/main/java/tn/esprit/tpfoyer/entity/Universite.java
@@ -0,0 +1,20 @@
+package tn.esprit.tpfoyer.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Universite {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idUniversite;
+    private String nomUniversite;
+    private String adresse;
+}
